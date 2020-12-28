ddb191c363543a7ddf05461de0ec09ecedd31dc5
张宇 <yu.zhang@higentec.com>
2020-12-28 10:45:03 +0800

    2020-12-28

--- springboot-demo/src/main/java/grimm/study/controller/TestController.java
+++ springboot-demo/src/main/java/grimm/study/controller/TestController.java
@@ -1,14 +1,21 @@
 package grimm.study.controller;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
 import grimm.study.exception.CommonException;
 import grimm.study.vo.ServerResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("/test")
-
+@Slf4j
 public class TestController {
 
 
@@ -19,6 +26,32 @@ public class TestController {
             System.out.println(1/0);
         }catch (Exception e){
             throw  new CommonException(e.getMessage());
+        }
+
+        return ServerResponse.success();
+    }
+
+    @RequestMapping("/logTest")
+    public ServerResponse logTest(){
+
+        log.trace("----trace----");
+        log.debug("----debug----");
+        log.info("----info----");
+        log.error("----error----");
+
+        return ServerResponse.success();
+    }
+
+    @RequestMapping(value = "{logLevel}",method = {RequestMethod.POST})
+    public ServerResponse setLogLevel(@PathVariable String logLevel){
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        Logger logger = loggerContext.getLogger("root");
+        if (logLevel.equals(Level.INFO.toString())){
+            ((ch.qos.logback.classic.Logger) logger).setLevel(Level.INFO);
+        }else if (logLevel.equals(Level.DEBUG.toString())){
+            ((ch.qos.logback.classic.Logger)logger).setLevel(Level.DEBUG);
+        }else if (logLevel.equals(Level.ERROR.toString())){
+            ((ch.qos.logback.classic.Logger)logger).setLevel(Level.ERROR);
         }
 
         return ServerResponse.success();
---
+++ springboot-demo/src/main/java/grimm/study/service/LogService.java
@@ -0,0 +1,4 @@
+package grimm.study.service;
+
+import org.springframework.stereotype.Service;
+
